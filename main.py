ff4e768b1aa7a86f15efd6e181b8abf82eb7944c
SmackingMyLemon <chhpearson@gmail.com>
2026-04-17 15:17:22 -0500

    Added player and movement logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import *
 
 def main():
     pygame.init()
@@ -9,6 +10,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+
+
+
 
     while True:
         log_state()
@@ -16,7 +30,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for thing in drawable:
+            thing.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
